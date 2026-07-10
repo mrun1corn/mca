7e3bad6ef786b70976e8559bdfe093de6e40969c
mrun1corn <robinisalegend01@gmail.com>
2026-07-10 23:34:06 +0600

    feat: enable vercel web analytics

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
+import { Analytics } from "@vercel/analytics/react";
 import "./styles.css";
 
 const qc = new QueryClient();
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={qc}>
       <BrowserRouter>
         <App />
+        <Analytics />
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
